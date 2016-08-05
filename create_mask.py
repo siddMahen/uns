e949001b08e05fac4003ca449e87289baead39b7
Siddharth Mahendraker <siddharth.mahen@gmail.com>
2016-08-05 19:02:17 +0300

    Try aggresive sampling

--- create_mask.py
+++ create_mask.py
@@ -3,7 +3,6 @@ import numpy as np
 from PIL import Image
 
 from model import inference
-from eval import prediction
 
 import sys
 import glob
@@ -21,13 +20,11 @@ def create_mask(run_name, filename):
     padded_im = np.pad(im, k, pad_zeros)
 
     with tf.Graph().as_default():
-
         images = tf.placeholder(tf.float32, shape=(1, PATCH_WIDTH, PATCH_WIDTH, 1))
         keep_prob = tf.Variable(1.0, name='keep_prob', trainable=False)
 
         logits = inference(images, keep_prob)
         prob = tf.nn.softmax(logits)
-        pred = prediction(logits)
 
         sess = tf.Session()
         saver = tf.train.Saver()
@@ -42,25 +39,24 @@ def create_mask(run_name, filename):
         saver.restore(sess, ckpt_path)
         keep_prob.assign(1.0)
 
-        #result = np.zeros((420, 580))
-        result = np.zeros((105, 145))
+        #heatmap = np.zeros((84, 116))
+        heatmap = np.zeros((21, 29))
 
         # try only looking at every fourth pixel?
-        patch = np.zeros((PATCH_WIDTH, PATCH_WIDTH))
-        for x in range(k, 420 + k, 4):
-            for y in range(k, 580 + k, 4):
-                for i in range(PATCH_WIDTH):
-                    for j in range(PATCH_WIDTH):
-                        patch[i,j] = padded_im[x + (i - k), y + (j - k)]
-                res = sess.run(prob, feed_dict={
-                    images: [patch.reshape((PATCH_WIDTH, PATCH_WIDTH, 1))] })
-                result[(x - k)/4, (y - k)/4] = res[0][1]
+        for x in range(0, 420, 20):
+            for y in range(0, 580, 20):
+                patch = padded_im[x:x+PATCH_WIDTH, y:y+PATCH_WIDTH].reshape((PATCH_WIDTH,
+                    PATCH_WIDTH, 1))
+                p = sess.run(prob, feed_dict={ images: [patch] })
+                heatmap[x/20, y/20] = p[0][1]
 
-        result = 255*result
-        im = Image.fromarray(result.astype('uint8'))
-        im.save('mask-test.tif')
-        #ar = np.reshape(255*ar, (420, 580))
-        #im = Image.fromarray(ar, mode='L').convert('1')
+        # could be (12,11) below
+        #heatmap = np.pad(255*heatmap, ((8,8), (11,12)), pad_zeros)
+        heatmap = 255*heatmap
+        print(heatmap.shape)
+
+        im = Image.fromarray(heatmap.astype("uint8"))
+        im.save("image_mask.tif")
 
         sess.close()
 
