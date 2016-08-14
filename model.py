8b966231e18c82849e67c207c7a93fcf7376cc55
Siddharth Mahendraker <siddharth.mahen@gmail.com>
2016-08-14 13:07:11 +0300

    Increased model complexity w/ skip connection
    Improtantly, reduces upsampling stride
    from ~16 to ~8, allowing for finer detail in
    upsampled mask.

--- model.py
+++ model.py
@@ -220,14 +220,41 @@ def inference(images, keep_prob, batch_size):
         l9_proj = conv_layer(l9_mp, 832, 128, k=1, s=1, layer_name="l9_proj")
 
         l9 = tf.concat(3, [l9_1x1, l9_3x3, l9_5x5, l9_proj])
-        # output here is 7 x 7 x 1024
+        # output here is 25 x 35 x 1024
 
-    deconv_1x1_pre = conv_layer(l9, 1024, 256, k=1, s=1, layer_name="deconv_1x1_pre")
+    l5_cpy = tf.identity(l5)
+    deconv_l5_1x1_pre = conv_layer(l5_cpy, 512, 512, k=1, s=1, layer_name="deconv_l5_1x1_pre")
+    #output here is 27 x 37 x 256
 
-    W = weight_var("deconv_weight", [32, 32, 2, 256])
+    W_l5 = weight_var("deconv_l5_weight", [10, 10, 512, 512])
+    deconv_l5 = tf.nn.conv2d_transpose(deconv_l5_1x1_pre,
+            W_l5, [batch_size, 210, 290, 512], [1, 8, 8, 1])
 
-    deconv = tf.nn.conv2d_transpose(deconv_1x1_pre,
-            W, [batch_size, 420, 580, 2], [1, 17, 17, 1])
+    deconv_l9_1x1_pre = conv_layer(l9, 1024, 512, k=1, s=1, layer_name="deconv_l9_1x1_pre")
 
-    return deconv
+    W_l9 = weight_var("deconv_l9_weight", [18, 18, 512, 512])
+    deconv_l9 = tf.nn.conv2d_transpose(deconv_l9_1x1_pre,
+            W_l9, [batch_size, 210, 290, 512], [1, 8, 8, 1], padding="VALID")
+
+    #deconv_concat = tf.concat(3, [deconv_l5, deconv_l9])
+    deconv_concat = tf.add(deconv_l5, deconv_l9)
+
+    W_final = weight_var("deconv_final_weight", [2, 2, 2, 512])
+    deconv_final = tf.nn.conv2d_transpose(deconv_concat, W_final,
+            [batch_size, 420, 580, 2], [1, 2, 2, 1], padding="VALID")
+
+    #skip_l5 = tf.nn.max_pool(l5, ksize=[1, 3, 3, 1], strides=[1, 1, 1, 1],
+    #        padding='VALID')
+    #deconv_l5_1x1_pre = conv_layer(skip_l5, 512, 256, k=1, s=1, layer_name="deconv_l5_1x1_pre")
+    #deconv_1x1_pre = conv_layer(l9, 1024, 256, k=1, s=1, layer_name="deconv_1x1_pre")
+    #deconv = tf.nn.conv2d_transpose(deconv_1x1_pre,
+    #        W, [batch_size, 210, 290, 2], [1, 17, 17, 1])
+
+    #print("ALSO HERE")
+
+    #dc = tf.concat(3, [deconv_l5_1x1_pre, deconv_1x1_pre])
+    #W_f = weight_var("deconv_final", [32, 32, 2, 512])
+    #fin = tf.nn.conv2d_transpose(dc, W_f, [batch_size, 420, 580, 2], [1, 17, 17, 1])
+
+    return deconv_final
 
